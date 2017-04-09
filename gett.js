96ee2728f3650fe979e927d2986a53246c51343d
Senal Kularatne <senalk90@gmail.com>
2017-04-09 12:37:55 -0400

    First commit

---
+++ gett.js
@@ -0,0 +1,3 @@
+// var div = document.getElementById("textDiv");  
+// div.textContent = "Apple is an American multinational technology company headquartered in Cupertino, California";  
+// var text = div.textContent; 
